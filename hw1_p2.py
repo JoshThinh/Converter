d8f410daaaaf52dd195dd7abb4a76174602432c7
JoshThinh <joshua.thinh@gmail.com>
2026-09-06 16:22:36 -0700

    updated description

--- hw1_p2.py
+++ hw1_p2.py
@@ -1,12 +1,15 @@
 #Build a number-base converter supporting binary, decimal, octal, and hexadecimal.
 
-#Used 
+#User inputs the number they want to convert
 text = input("Number: ")
 
+#User enters the base value that the number is in
 base = int(input("What is base of number (2, 8, 10, 16): "))
 
+#creates a variable to store the text and base into a tuple
 number = int(text, base)
 
+#prints out the number in different bases
 print("Binary: ", bin(number))
 print("Octal: ", oct(number))
 print("Decimal: ", number)
